471e1cf34234d6b43a712ff48d8d38e8d42040c7
krumitsukhadiya9099 <137699173+krumitsukhadiya9099@users.noreply.github.com>
2024-09-15 20:27:05 +0530

    Math_in js

--- 6numbersandmaths.js
+++ 6numbersandmaths.js
@@ -16,3 +16,21 @@ const otherNumber1 = 1000000
 //------------------------------------------Maths----------------------------------------------------------
 
 console.log(Math);
+
+// console.log(Math.abs(-4.5)); //Only Nagative value to positive number
+// console.log(Math.round(-4.5)); // for RoundOFF values
+// console.log(Math.ceil(-4.5));  // for RoundOFF Celling value
+// console.log(Math.floor(-4.5)); // for RoundOFF Flore value
+// console.log(Math.min(4, 3, 5, 6, 2)); // for Find lowest value in Arrey
+// console.log(Math.max(4, 3, 5, 6, 2)); // for Find Higest value in Arrey
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+//Random value are give value as desimal of zero's form for ramdom abseloute value between 10 to 20 use next
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
