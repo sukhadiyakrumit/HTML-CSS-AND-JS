bb5566bfe5ff25fbbe2e382f71826ad5d16c8d17
krumitsukhadiya9099 <137699173+krumitsukhadiya9099@users.noreply.github.com>
2024-09-15 19:36:07 +0530

    NumberandMaths

---
+++ 6numbersandmaths.js
@@ -0,0 +1,18 @@
+// Numbers
+const score = 500
+//console.log(score);
+
+const balance = new Number(200.3365)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(4));
+
+const otherNumber1 = 1000000
+//console.log(otherNumber1.toLocaleString('en-IN'));
+
+//------------------------------------------Maths----------------------------------------------------------
+
+console.log(Math);
